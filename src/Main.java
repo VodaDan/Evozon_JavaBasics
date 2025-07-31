1f6f75acc9816cee8a80c6511bb2a566bb242c9f
Dan <daniel.voda@ext.evozon.com>
2025-07-31 09:56:19 +0300

    Assignment-Intro to Java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Dan");
+
+        System.out.println(2+5);
+
+        System.out.println((float)9/4);
+
+        System.out.println(-5+8*6);  //a
+        System.out.println((55+9)%9);  //b
+        System.out.println(20+ (-3)*5/8);  //c
+        System.out.println(5+15/3*2-8%3);  //d
+
+    }
+}
