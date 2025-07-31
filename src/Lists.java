48bf26064de6b046be26f728d118d9179ba2ab2a
Dan <daniel.voda@ext.evozon.com>
2025-07-31 17:52:58 +0300

    Assignment Lists: added Exercises 1-6 - Lists printing, manipulation, adding at specific index

---
+++ src/Lists.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+
+    public void listPrint(List list) {
+        for(Object x:list) {
+            System.out.print(x + ", ");
+        }
+        System.out.println();
+    } // Lists - Ex 1
+
+    public void listAddNumber(ArrayList<Integer> list, int number) {
+        list.add(number);
+    } // Lists - Ex 2
+
+    public void listPrintFromNumber(ArrayList<Integer> list, int number) {
+        for(int i = list.indexOf(number); i < list.size();i++) {
+            System.out.println(list.get(i));
+        }
+    } // Lists - Ex 3
+
+    public void listPrintReverted(ArrayList<Integer> list) {
+        for(int i = list.size()-1; i >= 0; i--) {
+            System.out.println(list.get(i));
+        }
+    } // Lists - Ex 4
+
+    public void listAddElementAtIndex(ArrayList<String> list, int index, String word) {
+        list.add(index,word);
+    } // Lists - Ex 5
+
+    public void listAddElementFirstIndex(ArrayList<String> list, String word) {
+        list.addFirst(word);
+    } // Lists - Ex 6
+
+}
